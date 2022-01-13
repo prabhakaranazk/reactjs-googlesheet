cfc09182320b4b67d65de1db515f8830fdbd7e20
prabhaazk <prabhakaran@optanium.com>
2022-01-13 21:28:48 +0530

    implemented get and post

--- src/App.js
+++ src/App.js
@@ -1,14 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>React with Google Sheet - Prabhakaran</p>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: '',
+       age: '',
+       salary: '',
+       hobby: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  submitHandler = e => {
+    console.log(this.state);
+
+    axios.post('https://sheet.best/api/sheets/879a79f3-8401-428a-b545-5fb5db3cc49f', this.state)
+    .then(response => {
+      console.log(response);
+      alert('successfully added')
+    })
+  }
+
+  getData = async() => {
+    console.log('data');
+    try {
+      const res = await fetch('https://sheet.best/api/sheets/879a79f3-8401-428a-b545-5fb5db3cc49f');
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+//   useEffect(() => {
+// getData();
+//   },[]);
+
+componentDidMount(){
+  this.getData();
 }
-
-export default App;
+  
+  render() {
+    const { name, age, salary, hobby } = this.state;   
+    return (
+      <Container fluid className="container">
+        <Header as='h2'>React Google Sheets!</Header>
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Salary</label>
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Hobby</label>
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
+          </Form.Field>
+          
+          <Button color="blue" type='submit'>Submit</Button>
+        </Form>
+        <br/>
+        {/* <Button color="green" onClick={this.getData}>Get Data</Button> */}
+      </Container>
+    )
+  }
+}
